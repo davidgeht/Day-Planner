cfb9126e72e359dadc4606871b72177fdd16a013
David Gehtman <davidgehtman.91@gmail.com>
2019-10-23 13:59:35 -0400

    test push

---
+++ javascript/jquery.js
@@ -0,0 +1,14 @@
+// detect current time function 
+$(document).ready(function(){
+    // create a var to call the id 
+    var currentDateEl = $("#currentDay");
+    // create a var to detect the current date
+    var curDate = moment().format('MMMM Do YYYY');
+    currentDateEl.text(curDate);
+});
+
+// function to save input from text area 
+$(document)
+
+
+}
