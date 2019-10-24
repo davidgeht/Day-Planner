912fc47d4e989ee529f16d95a8a86d119a35360b
David Gehtman <davidgehtman.91@gmail.com>
2019-10-24 17:49:57 -0400

    added current time function, added local storage function

--- javascript/jquery.js
+++ javascript/jquery.js
@@ -6,9 +6,15 @@ $(document).ready(function(){
     var curDate = moment().format('MMMM Do YYYY');
     currentDateEl.text(curDate);
 });
-
+// textbox intializer
+var schedArray = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
+for (var i = 0; i < schedArray.length; i++){
+       $(".container").append(`<div class="row time-block" id=block${schedArray[i]}><div class="col-2 hour" id=${schedArray[i]}hour><p>${schedArray[i]}</p></div> 
+       <textarea class="col-8 description" id=desc${schedArray[i]}></textarea><button class="col-2 saveBtn i far fa-save" id=btn${schedArray[i]}> </i></button></div>`);
+};   
 // function to save input from text area 
-$(document)
+$(".saveBtn").click(function(){
+    var textValue=$(this.parentNode.children[1]).val();
+    localStorage.setItem($(this).attr("id"),textValue);
+});
 
-
-}
